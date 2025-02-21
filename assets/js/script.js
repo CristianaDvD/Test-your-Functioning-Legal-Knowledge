033c7ba42f97b50f48a61ad01ec6aec095960b93
Cristiana David <cristiana.radu92@yahoo.com>
2025-02-21 21:10:22 +0000

    Add 5 scenarios in the first js const

---
+++ assets/js/script.js
@@ -0,0 +1,110 @@
+const scenarios = [
+  {
+    scenario:
+      "An architect received a leaflet from website designers advertising their website design packages. On the back of the leaflet was a copy of the website designers’ standard terms, which contained a limitation clause. The architect wrote a letter to the website designers asking them to design his website and he attached to his letter a copy of his own standard terms and conditions, which did not contain a limitation clause. The architect received a quotation for £2,500 from the website designers. The architect signed and returned a tear-off slip to the website designers which stated that he accepted the quotation on the website designers’ standard terms and conditions. Which of the following statements best describes the legal position? ",
+    answers: [
+        { text: "The terms on the leaflet constituted an offer which the architect accepted by asking for a quotation.", correct: false},
+        { text: "The quotation constituted an offer which the architect accepted on the website designers’ standard terms and conditions.", correct: true},
+        { text: "The quotation constituted an offer which the architect accepted on the architect’s standard terms and conditions.", correct: false},
+        { text: "The letter from the architect to the web designers constituted an offer which the web designers accepted by sending a quotation.", correct: false},
+        { text: "The letter from the architect to the web designers constituted a counter offer which the web designers accepted by sending a quotation.", correct: false}
+    ]
+    },
+
+    {
+        scenario: "Last month a cycling enthusiast incorporated an online bicycle accessories shop. He is the sole director and he and a friend are the only shareholders. Prior to incorporation of the company, the enthusiast negotiated a contract with a provider of cycling clothing. The contract was signed, prior to the receipt of the certificate of incorporation, by the enthusiast in his own name, on behalf of the company. With whom, if anyone, does the benefit of the contract reside?",       
+         answers: [
+            { text: "The company only.", correct: false},
+            { text: "The shareholders only", correct: false},
+            { text: "The enthusiast only.", correct: true},
+            { text: "The enthusiast and the company jointly.", correct: false},
+            { text: "No one, the contract is void.", correct: false}
+        ]
+        },
+
+        {
+            scenario: "A man is employed by a roofing company to repair roofs. The role requires him to wear special gloves in order to protect his hands when he handles roof tiles. Whilst standing on some scaffolding, negligently erected by the roofing company, he falls off, suffers serious injury to his head and brings a claim. He carelessly failed to wear the gloves at the time. Which of the following best describes whether the company can successfully claim contributory negligence on the part of the roofer? ",
+            answers: [
+                { text: "The company cannot claim contributory negligence because the roofer’s carelessness did not cause or contribute to the injury.", correct: true},
+                { text: "The company cannot claim contributory negligence because the roofer did not owe the company a duty of care.", correct: false},
+                { text: "The company can claim contributory negligence because the roofer’s carelessness caused or contributed to the injury.", correct: false},
+                { text: "The company can claim contributory negligence because the roofer’s carelessness materially increased the risk of injury.", correct: false},
+                { text: "The company can claim contributory negligence because the roofer owed the company a duty of care.", correct: false}
+            ]
+            },
+            {
+                scenario: "A private limited company has unamended model articles as set out in Schedule 1 to the Companies (Model Articles) Regulations 2008. The company has issued ordinary shares only and has no shareholders’ agreement. Its most recent set of accounts show that the company did not make a profit or a loss in the most recent accounting period, although it has distributable profits of £50,000 carried forward from previous accounting periods. The shareholders want the company to declare a dividend out of these profits, but the directors intend to retain the profits within the company to finance an expansion of the company’s trade. Can the shareholders compel the directors to recommend a dividend?",               
+                 answers: [
+                    { text: "Yes, because the directors have a duty to act in the best interests of the shareholders.", correct: false},
+                    { text: "Yes, because the shareholders have the power to require payment of a dividend by ordinary resolution.", correct: false},
+                    { text: "Yes, because the shareholders have a right to receive a dividend if the company has distributable profits.", correct: false},
+                    { text: "No, because only the directors have the power to recommend the payment of a dividend.", correct: true},
+                    { text: "No, because the directors can only recommend a dividend out of profits of the most recent accounting period. ", correct: false}
+                ]
+                },
+                {
+                    scenario: "A man is arrested and detained at a police station on suspicion of common assault which is a summary only offence. The man is homeless and has no income. The man is not legally represented during his interview under caution, in which he denies the offence. After the interview the man is told that he will be charged with the offence. The man states that he now wants legal advice as he does not want the police to charge him. Is the man entitled to publicly funded legal advice at the police station?",                    
+                     answers: [
+                        { text: "The terms on the leaflet constituted an offer which the architect accepted by asking for a quotation.", correct: false},
+                        { text: "The quotation constituted an offer which the architect accepted on the website designers’ standard terms and conditions.", correct: true},
+                        { text: "The quotation constituted an offer which the architect accepted on the architect’s standard terms and conditions.", correct: false},
+                        { text: "The letter from the architect to the web designers constituted an offer which the web designers accepted by sending a quotation.", correct: false},
+                        { text: "The letter from the architect to the web designers constituted a counter offer which the web designers accepted by sending a quotation.", correct: false}
+                    ]
+                    },
+                    {
+                        scenario:
+                          "An architect received a leaflet from website designers advertising their website design packages. On the back of the leaflet was a copy of the website designers’ standard terms, which contained a limitation clause. The architect wrote a letter to the website designers asking them to design his website and he attached to his letter a copy of his own standard terms and conditions, which did not contain a limitation clause. The architect received a quotation for £2,500 from the website designers. The architect signed and returned a tear-off slip to the website designers which stated that he accepted the quotation on the website designers’ standard terms and conditions.",
+                        answers: [
+                            { text: "The terms on the leaflet constituted an offer which the architect accepted by asking for a quotation.", correct: false},
+                            { text: "The quotation constituted an offer which the architect accepted on the website designers’ standard terms and conditions.", correct: true},
+                            { text: "The quotation constituted an offer which the architect accepted on the architect’s standard terms and conditions.", correct: false},
+                            { text: "The letter from the architect to the web designers constituted an offer which the web designers accepted by sending a quotation.", correct: false},
+                            { text: "The letter from the architect to the web designers constituted a counter offer which the web designers accepted by sending a quotation.", correct: false}
+                        ]
+                        },
+                        {
+                            scenario:
+                              "An architect received a leaflet from website designers advertising their website design packages. On the back of the leaflet was a copy of the website designers’ standard terms, which contained a limitation clause. The architect wrote a letter to the website designers asking them to design his website and he attached to his letter a copy of his own standard terms and conditions, which did not contain a limitation clause. The architect received a quotation for £2,500 from the website designers. The architect signed and returned a tear-off slip to the website designers which stated that he accepted the quotation on the website designers’ standard terms and conditions.",
+                            answers: [
+                                { text: "The terms on the leaflet constituted an offer which the architect accepted by asking for a quotation.", correct: false},
+                                { text: "The quotation constituted an offer which the architect accepted on the website designers’ standard terms and conditions.", correct: true},
+                                { text: "The quotation constituted an offer which the architect accepted on the architect’s standard terms and conditions.", correct: false},
+                                { text: "The letter from the architect to the web designers constituted an offer which the web designers accepted by sending a quotation.", correct: false},
+                                { text: "The letter from the architect to the web designers constituted a counter offer which the web designers accepted by sending a quotation.", correct: false}
+                            ]
+                            },
+                            {
+                                scenario:
+                                  "An architect received a leaflet from website designers advertising their website design packages. On the back of the leaflet was a copy of the website designers’ standard terms, which contained a limitation clause. The architect wrote a letter to the website designers asking them to design his website and he attached to his letter a copy of his own standard terms and conditions, which did not contain a limitation clause. The architect received a quotation for £2,500 from the website designers. The architect signed and returned a tear-off slip to the website designers which stated that he accepted the quotation on the website designers’ standard terms and conditions.",
+                                answers: [
+                                    { text: "The terms on the leaflet constituted an offer which the architect accepted by asking for a quotation.", correct: false},
+                                    { text: "The quotation constituted an offer which the architect accepted on the website designers’ standard terms and conditions.", correct: true},
+                                    { text: "The quotation constituted an offer which the architect accepted on the architect’s standard terms and conditions.", correct: false},
+                                    { text: "The letter from the architect to the web designers constituted an offer which the web designers accepted by sending a quotation.", correct: false},
+                                    { text: "The letter from the architect to the web designers constituted a counter offer which the web designers accepted by sending a quotation.", correct: false}
+                                ]
+                                },
+                                {
+                                    scenario:
+                                      "An architect received a leaflet from website designers advertising their website design packages. On the back of the leaflet was a copy of the website designers’ standard terms, which contained a limitation clause. The architect wrote a letter to the website designers asking them to design his website and he attached to his letter a copy of his own standard terms and conditions, which did not contain a limitation clause. The architect received a quotation for £2,500 from the website designers. The architect signed and returned a tear-off slip to the website designers which stated that he accepted the quotation on the website designers’ standard terms and conditions.",
+                                    answers: [
+                                        { text: "The terms on the leaflet constituted an offer which the architect accepted by asking for a quotation.", correct: false},
+                                        { text: "The quotation constituted an offer which the architect accepted on the website designers’ standard terms and conditions.", correct: true},
+                                        { text: "The quotation constituted an offer which the architect accepted on the architect’s standard terms and conditions.", correct: false},
+                                        { text: "The letter from the architect to the web designers constituted an offer which the web designers accepted by sending a quotation.", correct: false},
+                                        { text: "The letter from the architect to the web designers constituted a counter offer which the web designers accepted by sending a quotation.", correct: false}
+                                    ]
+                                    },
+                                    {
+                                        scenario:
+                                          "An architect received a leaflet from website designers advertising their website design packages. On the back of the leaflet was a copy of the website designers’ standard terms, which contained a limitation clause. The architect wrote a letter to the website designers asking them to design his website and he attached to his letter a copy of his own standard terms and conditions, which did not contain a limitation clause. The architect received a quotation for £2,500 from the website designers. The architect signed and returned a tear-off slip to the website designers which stated that he accepted the quotation on the website designers’ standard terms and conditions.",
+                                        answers: [
+                                            { text: "The terms on the leaflet constituted an offer which the architect accepted by asking for a quotation.", correct: false},
+                                            { text: "The quotation constituted an offer which the architect accepted on the website designers’ standard terms and conditions.", correct: true},
+                                            { text: "The quotation constituted an offer which the architect accepted on the architect’s standard terms and conditions.", correct: false},
+                                            { text: "The letter from the architect to the web designers constituted an offer which the web designers accepted by sending a quotation.", correct: false},
+                                            { text: "The letter from the architect to the web designers constituted a counter offer which the web designers accepted by sending a quotation.", correct: false}
+                                        ]
+                                        },                                 
+];
